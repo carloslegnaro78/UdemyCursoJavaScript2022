3dab1772417193c3755b06ccf2c8cd2881592a38
carlos.legnaro <carlos.legnaro@outlook.com>
2022-10-22 11:16:37 -0300

    Bloco de Código - Aula 20

---
+++ comentario.js
@@ -0,0 +1,12 @@
+// console.log é usuado para exibir algo na tela do computador!
+console.log('Passo #03');
+console.log('Passo #01');
+
+/*
+    Esse é um 
+    comentário
+    de múltiplas
+    linhas
+*/
+
+console.log('Passo #02');
