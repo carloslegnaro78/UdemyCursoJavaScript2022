c0eb836110df6954c48c576f6ec1dc615497c264
carlos.legnaro <carlos.legnaro@outlook.com>
2022-10-25 21:15:04 -0300

    Ajuste de Troca.

--- desafio_troca.js
+++ desafio_troca.js
@@ -1,12 +1,13 @@
 let a = 7;
 let b = 94;
 
-let temp = a;
-
-a = b;
-b = temp;
+//let temp = a;
+//a = b;
+//b = temp;
 
 // depois da troca ... a = 94 e b = 7
+
+[a,b] = [b, a]
 
 console.log(a);
 console.log(b);
