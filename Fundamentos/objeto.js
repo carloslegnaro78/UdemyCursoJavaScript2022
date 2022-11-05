ed3fdfe8aa1a1c207ed43a35b3a5a3d0c54bfd3d
carlos.legnaro <carlos.legnaro@outlook.com>
2022-11-04 22:42:13 -0300

    correção...

--- Fundamentos/objeto.js
+++ Fundamentos/objeto.js
@@ -8,13 +8,10 @@ console.log(prod1)
 
 const prod2 = {
     nome: 'Camisa Polo',
-    preco: 79.90,
-    obj: {
-        blabla: 1,
-        obj: {
-            blabla2: 2
-        }
-    }
+    preco: 79.90
+   
 }
+
+
 
 console.log(prod2)
