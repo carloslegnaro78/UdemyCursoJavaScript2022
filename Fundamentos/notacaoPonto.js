c504ff59b3d3917c9aa8c90a7ed59ccde8c3ff70
carlos.legnaro <carlos.legnaro@outlook.com>
2022-11-10 23:03:39 -0300

    ajuste

--- Fundamentos/notacaoPonto.js
+++ Fundamentos/notacaoPonto.js
@@ -4,3 +4,12 @@ const obj1 = {}
 obj1.nome ='Bola'
 //obj1['nome'] = 'Bola2' as duas anotações são validas
 console.log(obj1.nome)
+
+function Obj(nome){
+    this.nome = nome
+}
+
+const obj2 = new Obj('Cadeira')
+const obj3 = new Obj('Mesa')
+console.log(obj2.nome)
+console.log(obj3.nome)
