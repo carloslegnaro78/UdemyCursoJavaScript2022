ec5eeefb4f4084317c86a5d2deb1f7c65ad3e5d2
carlos.legnaro <carlos.legnaro@outlook.com>
2022-11-15 23:15:35 -0300

    forma reduzida

--- Fundamentos/logico.js
+++ Fundamentos/logico.js
@@ -5,10 +5,10 @@ function compras (trabalho1, trabalho2)
     const comprarTv32 = !!(trabalho1 ^ trabalho2)  //bitwise xor
     const manterSaudavel = !comprarSorvete // operador    
 
-    return {
-        comprarSorvete : comprarSorvete,
-        comprarTv50: comprarTv50,
-        comprarTv32: comprarTv32,
-        manterSaudavel : manterSaudavel 
-    }
+    return { comprarSorvete, comprarTv50, comprarTv32, manterSaudavel }
 }
+
+console.log(compras(true, true))
+console.log(compras(true,false))
+console.log(compras(false,true))
+console.log(compras(false, false))
